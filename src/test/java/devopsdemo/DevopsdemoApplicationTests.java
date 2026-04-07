bed07a1d8fea4487f87ca88459522b5b2f5d4501
harishjangam235 <harishjagam235.com>
2026-04-07 11:35:28 +0530

    Initial DevOps Micro SaaS Project - Harish

---
+++ src/test/java/devopsdemo/DevopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
